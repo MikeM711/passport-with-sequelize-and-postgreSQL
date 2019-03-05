b3687ac06edba11647b20be1318b6f463bb3b760
Michael <michaelmccabe711@gmail.com>
2019-03-05 15:55:19 -0500

    Create the User Model

---
+++ app/models/user.js
@@ -0,0 +1,128 @@
+/* 
+QUESTIONS:
+    1. Can we substitute all 'notEmpty' for 'allowNull' validations?
+    2. last_login timestamp equal to createdAt/updatedAt data types?
+    3. Can we write validations with/witout the 'validations' object?
+*/
+
+/*
+2.1 UTILITY:
+This file gives us access to methods such as 
+- findAll()
+- create()
+- update() 
+- destroy()
+*/
+
+/* Our module will be automatically exported into index.js 'model', and will need the use of 'sequelize' and 'Sequelize' to work
+    - sequelize to use the define() method
+    - Sequelize to use the table field data types
+The below 'module.export' line is used for ALL models 
+*/
+module.exports = function (sequelize, Sequelize) {
+
+    /* 
+    The name of the const variable will be the name of our model (capitalized & singular)
+        Set our model = sequelize.define() to define mappings between model and table
+            First param = SINGULAR name of the database table
+                That singular name will become plural when inserted into postgres
+            Second param = the sequelize model that represents the database table, as an object
+    */
+    
+    const User = sequelize.define('user', {
+
+        /* Database field structure:
+            - All have data types
+            - Use validation whenever you can
+            - Can be empty, unless otherwise stated
+        */
+
+        /* id is our primary key, of a number (integer) that will increment up (autoIncrement)
+            If not set, postgres will create one by itself */
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+
+        /* firstname/lastname: 
+            data type: character varying (255)
+            validation: don't allow empty strings
+        */
+
+        firstname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+
+        lastname: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+
+        /* username/about:
+            data type: character varying + no length limit
+        */
+
+        username: {
+            type: Sequelize.TEXT
+        },
+
+        about: {
+            type: Sequelize.TEXT
+        },
+
+        /* email:
+            data type: character varying (255)
+            validation: Check for email format (Ex: foo@bar.com)
+                I want to say that you can use either a validation object, or just the 'isEmail' validation by itself...
+        */
+
+        email: {
+            type: Sequelize.STRING,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        /* password:
+            data type: character varying (255)
+            validation: don't allow null values
+                This is typical for 'password' fields
+        */
+
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+
+        /* last_login:
+            data type: timestamp of an instance
+                Question - Exactly the same as 'createdAt' and 'updatedAt'?
+        */
+
+        last_login: {
+            type: Sequelize.DATE
+        },
+
+        /* status:
+            data type: An ENUM, where we declared the only legal values
+                The only allowed values are'active' and 'inactive'
+            model definition: When a user logs in, they will be 'active' 
+        */
+
+        status: {
+            type: Sequelize.ENUM('active', 'inactive'),
+            defaultValue: 'active'
+        }
+
+        /*
+        Sequelize will then automatically add the attributes 'createdAt' and 'updatedAt' to this model, and thus, to the database
+        */
+
+    });
+
+    // Return the variable out of the function
+    return User;
+
+}
--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 
 // 1.1 Create a path to our models
-
 /* In COMPLETED "todo" project this is called "db"
     Best to have our app/config/config.json in our .gitignore
 */
@@ -13,7 +12,6 @@ const models = require("./app/models");
 const app = express();
  
 // 0.5 Send some text to the browser
-
 /* 
 Use app.get() to "match and handle a specific route when requested"
   Parameter 1: The path
@@ -27,7 +25,6 @@ app.get('/', (req, res) => {
 });
 
 // 0.3. Create a port
-
 /*
 If we host this application on a service, we will need process.env.PORT
   Otherwise '||' , we will listen to localhost:5000, '5000'
@@ -38,9 +35,11 @@ const PORT = process.env.PORT || 5000;
 // If local, we will be listening on PORT '5000'
 app.listen(PORT, (err) => {
 
-  // if the error is undefined (server is working), let the developer know in the console 
+  // 0.5 if the error is undefined (server is working), let the developer know in the console 
   if (!err) {
-    // 1.2 If no error - Using the model path, look through all of our models and replicate them to the database
+    /* 1.2 Create the database in postgres (the 'database' we used in our config.json)
+      We just need to create the name, our models will automatically create our tables for EZ work
+    If no error - Using the model path, look through all of our models and replicate them to the database */
     models.sequelize.sync()
       // 1.3 Send messages to developer, for whether or not the database is in sync with our models
       .then(() => {
@@ -49,9 +48,11 @@ app.listen(PORT, (err) => {
       .catch((err) => {
       console.log(err, "Something went wrong with the Database Update!")
       });
+    
+    // 0.6 error is undefined - server is working properly
     console.log("Site is live");
   }
-  // if the error IS defined (server is not working), let the developer know in the console, and send in the error message
+  // 0.7 if the error IS defined (server is not working), let the developer know in the console, and send in the error message
   else console.log(err)
 
 });
