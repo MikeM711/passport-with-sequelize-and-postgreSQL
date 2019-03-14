833044b957c399c62b3fdd68488157d3ff83da9a
Michael <michaelmccabe711@gmail.com>
2019-03-14 16:28:11 -0400

    Ability to attach error messages to request object using req.flash('messageArr'), installed connect-flash and initialized it. A catch for 'local-signup' strategy to handle errors due to database/model validation failure. 'Model' notes on the use of 'allowNull' and 'validate' object. Extra HTML validation - 'required' attribute. In-depth notes about passport strategies - What happens on 'signup' and 'signin'. Created simple '/greet' route.

--- app/routes/auth/index.js
+++ app/routes/auth/index.js
@@ -19,7 +19,26 @@ Create a GET route to display a view (GET can also fetch data from inside a data
             - Remember: The application knows this because we set handlebars extensions as the default template engine
 */
 router.get('/signup', (req, res) => {
-    res.render('signup');
+    // 5.1 Grab the connect-flash error messages so we can display them on the "signup.hbs" view
+
+    // When we click "sign up" submit button, we end up in the local-signup strategy ('/signupPOST' handler) immediately
+
+    /*If we do NOT enter an email and password - execution IMMEDIATELY FAILS on the strategy
+        Giving us: failureRedirect: '/signup'
+        Passport MUST REQUIRE some type of 'username' AND 'password' in order to function
+            Extra info: These 2 fields can have their names defaulted to different names in the first object of the strategy, if needed
+                Using: usernameField: 'newFieldName1' and passwordField: 'newFieldName2'
+    */
+ 
+    /* Inside our "strategy", we can say: req.flash('arrayName','message stored in array')
+        Right here, in our "routes", grab the messsages by saying:  req.flash('arrayName') - this is the literal array of error messages
+            So, treat this as a literal array*/
+    
+    /* store the FIRST error message inside a variable and send it to the view
+        Our application will stop executing at the first error it receives,
+            so storing only the first error message makes sense */
+    const errorMessage = req.flash('messages')[0]
+    res.render('signup', {errorMessage});
 });
 
 // 3.3.1 Do the same for the sign in page - '/site/auth/signin'
@@ -51,6 +70,17 @@ function isLoggedIn(req, res, next) {
     // if 'isAuthenticated' is 'undefined', redirect the user to '/signin' URL page
     res.redirect('/signin');
 }
+
+// 5.1 This simple route works too!
+router.get('/greet', (req,res) => {
+    if(req.isAuthenticated()){
+        const user = req.user
+        res.send(`greetings ${user.firstname}, we are in this route!`)
+    } else {
+        res.send('sorry, you are not authenticated.')
+    }
+    
+})
 
 // 4.3 Dashboard route
 
@@ -105,22 +135,137 @@ router.get('/logout', (req, res) => {
             We want to use that 'local-signup' block of code to authenticate our /signupPOST
         Second param: An object of redirects and flash message options
 */
+
+/* What exactly is happening on signup?
+    1. User fills out some form information
+        If all of the HTML validations are passed, the action goes to '/signupPOST' handler as a method of 'POST'
+            As we are being sent to this URL, all <input> names and values are follow!
+    2. Execution enters here, it goes right into the callback: passport.authenticate('local-signup',{...})
+    3. Execution jumps right into passport.js 'local-signup' strategy
+    4. Verify Callback function IMMEDIATELY looks for a 'username' and 'password' field as the <input> name
+        We can override default with our OWN field names
+            If you do this, make sure you modify the parameters in the Verify Callback Function to match "overridden default"
+        If these field names have empty strings, execution comes back here and into failureRedirect
+        Override default with 'passReqToCallback: true', to make use of the 'req' object
+            If you do this, make sure you add the 'req' parameter to the Verify Callback Function
+            Also, enable bodyParser so that we can grab MORE form information: 'req.body.formInputNameAttribute' !!
+                We will now have access to more than just the 'username' and 'password' fields, gathered by the passport strategy!
+    5. *create hash function with bCrypt, don't use it yet*
+    6. Find the 'username' in the database - Check that the username IS unique - Model.findOne({where: {username: username}})
+        If it doesn't exist (undefined return): the username is unique, continue with strategy
+        If it does exist (defined return): the username is taken, write done() with a defined param1 or param3 to jump out of strategy
+            Param 1: shows blank page on 'POST' URL that displays the variable
+            Param 3: Immediately enter failureRedirect with a req.flash() message (if we choose to use it)
+        No real need for a .catch(), because success happens if the search finds something, or it searches every row
+            Maybe for the case of no database, we may need a catch()?
+    7. The user has input a unique username - hash the password with bCrypt
+        Get user-inputted 'password' from the parameter of the Verify Callback function
+    8. Create a data object, where that data object has data that matches our database table (model) of "User"
+        "databaseField: valueOfField"
+        Get 'username' from the parameter of the Verify Callback function
+        Use "Hashed Password"
+        Get other <input> values using req.body.formInputName
+    9. Send the data to the database - Model.create(data)
+        If success: the database table has OFFICIALLY added a row of information for the new user!
+            Thus, we passed all of our constraints and validations inside our model!
+            I want to say that we have successfully added the user to the database at this point
+        If failure: We have a database validation error - which we set up in our "model"
+            Supply done() a defined 1st param or 3rd param to exit out of this "passport system" as an error
+    10. Database addition of the new user is successful
+        Supply done() with a defined 2nd param to let passport know that it can keep going
+        That 2nd parameter of done() will be supplied to passport.serializeUser
+            If we are doing this appropriately, that 2nd param will be CONCISE user database information
+    11. serializeUser has access to the 2nd param
+            Send the User ID (made from the database) into the 2nd parameter of done(), to let passport know to continue
+                That 2nd param will be the user.id
+        With the help of app.use(session({ secret: 'secret key', ... })), we will be creating a cookie
+    12. We enter deserializeUser - we currently have a cookie that has an authentication token
+            If this passport process ends up being successful, 
+                this "authentication token" on this cookie will allow access to all routes that have 'isAuthenticated()' criteria, until the user's session ends
+    13. deserializeUser provides us with that same ID from inside the cookie, find that particular ID in the database
+            Use: 'Model.findById(id)'
+                Success is defined by finding the 'id', returning the full "database row information", 
+                        or finishing the full 'id' search, thus returning nothing
+                    If return is defined: We have found the 'user' in our database and we have access to the "db row information"
+                    If return is undefined: We have not found the 'user' in our database
+                        Not sure how execution would ever be able to make it here...
+    14. We have found the 'user' in our database, take the returning "db row information", make it concise, 
+        and send it as the 2nd param of the done() method to let passport know that it can continue
+            The 'defined' 2nd param will be the concise user information, which is important for the next stage of passport
+            Remember: we acquired the ID from the cookie, and we got the db information using that ID from the cookie
+    15. Completing the passport system is a success
+        Passport enters 'successRedirect' as long as the 2nd param of the done() of deserializing is defined
+            That's the only criteria!!
+        Whatever that 2nd parameter is, will be = 'req.user' inside ALL OF OUR ROUTES!!
+            You can use 'req.user' anywhere that has the callback (req,res) => {...}
+        Because this cookie and session are now working,  the cookie's "authentication token" 
+            causes 'req.isAuthenticated()' to be 'true' throughout the entire session!
+                It's like a key that will always unlock the 'req.isAuthenticated()' criteria
+        Nice.
+*/
 router.post('/signupPOST', passport.authenticate('local-signup', {
-    // 4.2 The below occurs when done() method is called inside deserialize with a defined 1st or 2nd param
-        // defined 1st param = error (failure), defined 2nd param = user data (success)
+    /* 4.2 Execution enters here when done() method is called inside deserialize, with a defined 2nd param
+                Here, execution will enter 'successRedirect', regardless if the user information is correct or not
+                    The 2nd param is defined, that's all that matters to be SUCCESSFUL!
+            If we ever have a defined 3rd param: execution immediately enters here, under failureRedirect + req.flash() message (if we choose to use it)
+            If we ever have a defined 1st param, this "authenticate system of stages" will IMMEDIATELY quit out all of all "stages"
+                User will land on this URL '/signupPOST' with a blank page, rendering out the 1st param
+        We get an instant failureRedirect if the user does not provide an email and password */
+        /* done: 
+            defined 1st param = error (instant failure, displays a blank screen with that variable on that web page, with the URL of the 'POST' request ), 
+            defined 2nd param = Defined info, telling passport that it's ready to go to the next stage of passport with this particular information
+                For deserailize: That particular defined info would be 'user data', which gets sent to the next stage of passport
+            defined 3rd param = error (instant failure, but enters 'failureRedirect' (unlike Param1 which stays inside 'POST' URL) 
+                with a req.flash('arrayName') variable that we can use )
+        */
+    /* User will be redirected to the below location with an "authenticated session" - isAuthenticated() = true;
+        This 'req.isAuthenticated() = true', that lasts the whole session (thanks to passport), 
+            helps execution pass through the "authenticated" criteria that a private route may have */
     successRedirect: '/dashboard',
-    failureRedirect: '/signup'
+    /* User will be redirected to the below location, with no change in authentication
+        'req.isAuthenticated()' is still 'false' */
+    failureRedirect: '/signup',
+    /* use the below if you want to use the 3rd parameter of done() (error messages), 
+        and be able to use req.flash('arrayName') to access these error messages 
+        Criteria: connect-flash should be installed and initialized within the app first, and have 'passReqToCallback: true' inside strategy 
+            and have the below 'failureFlash' */
+    failureFlash: true 
     }
 ));
 
 // 4.7 - similar to '/signupPOST' handler
 // Create a route handler for when a user, inside the signin.hbs "view", clicks the "submit" of the form, and the form 'action' begins
 // Again, just like /signupPOST: the router.post() path parameter should equal the 'action' path attribute in the signin.hbs "view"
+
+/* What exactly is happening on signin? - *VERY* similar to 'local-signup' strategy
+    1-4. is roughly the same
+    5. *create a function to compare hash password in database with user's plain password using bCrypt, don't use it yet*
+    6. Find the 'username' in the database - Check that the username IS unique - Model.findOne({where: {username: username}})
+        In our case, we are using 'email'
+        If email is found, we will continue the strategy
+    7. The email is found, but does the password on the database equal the password from the user?
+        Unhash the database password, using bCrypt, and compare it against the user's input 'password'
+        Syntax: bCrypt.compareSync(plainTextPW, hash)
+        If passwords compare, continue the strategy
+    8. We know that the email is found in the database, and that the hash-password matches the plain-password
+        *Exactly* like how we exited out of the strategy with 'local-signup':
+            Supply done() with a defined 2nd param to let passport know that it can keep going
+        That 2nd parameter of done() will be supplied to passport.serializeUser
+            If we are doing this appropriately, that 2nd param will be CONCISE user database information
+    10-15. exactly like 'local-signup'
+        Whatever that 2nd parameter is inside deserializeUser, will be = 'req.user' inside ALL OF OUR ROUTES!!
+                You can use 'req.user' anywhere that has the callback (req,res) => {...}
+            Because this cookie and session are now working, the cookie's "authentication token" 
+                causes 'req.isAuthenticated()' to be 'true' throughout the entire session!
+                    It's like a key that will always unlock the 'req.isAuthenticated()' criteria
+        Nice.
+*/
 router.post('/signinPOST', passport.authenticate('local-signin', {
     // 4.7 The below occurs when done() method is called inside deserialize with a defined 1st or 2nd param
         // defined 1st param = error (failure), defined 2nd param = user data (success)
     successRedirect: '/dashboard',
-    failureRedirect: '/signin'
+    failureRedirect: '/signin',
+    failureFlash: true
     }
 ));
 
